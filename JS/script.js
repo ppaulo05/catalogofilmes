db025d4da96646d415ffdeeb14849f7327599319
paulo <paulo-engelmann@educar.rs.gov.br>
2023-03-13 11:16:30 -0300

    parte 2

--- JS/script.js
+++ JS/script.js
@@ -44,24 +44,35 @@
 
 //console.log(filme);
 
+//btnBuscarFilme.onclick = () => {
+//  if (inputBuscarFilme.ariaValueMax.lenght > 0) {
+//      fetch("http://www.omdbapi.com/?apikey=88ffc734&s=" + inputBuscarFilme.value, { mode: "cors" })
+//          .then((resp) => resp.json())
+//          .then((resp) => {
+//              console.log(resp);
+//          });
+//  }
+//  return false;
+//}
+
 let inputBuscarFilme = document.querySelector("#input-buscar-filme");
 let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
 
-btnBuscarFilme.onclick = function () {
-    if (inputBuscarFilme.ariaValueMax.lenght > 0) {
-        fetch("http://www.omdbapi.com/?i=tt3896198&apikey=88ffc734" + inputBuscarFilme.value, { mode: "cors" })
-            .then((resp) => resp.json())
-            .then((resp) => {
-                console.log(resp);
-            });
-    }
-    return false;
+let listarFilmes = async (filmes) => {
+  let listaFilmes = document.querySelector("#lista-filmes");
+  listaFilmes.innerHTML = "";
+  console.log(listaFilmes);
+  if(filmes.length > 0) {
+    filmes.forEach(async(filme) => {
+      listaFilmes.appendChild(await filme.getCard());
+    });
+  }
 }
 
-btnBuscarFilme.onclick = async () => {
+btnBuscarFilme.onclick = () => {
     if(inputBuscarFilme.value.length > 0){
       let filmes = new Array();
-      fetch("http://www.omdbapi.com/?i=tt3896198&apikey=88ffc734"+inputBuscarFilme.value)
+      fetch("http://www.omdbapi.com/?apikey=88ffc734&s="+inputBuscarFilme.value)
       .then((resp)=> resp.json())
       .then((resp)=> {
         resp.Search.forEach((item)=>{
@@ -90,13 +101,4 @@ btnBuscarFilme.onclick = async () => {
     return false;
 }
 
-let listarFilmes = async (filmes) => {
-  let listaFilmes = await document.querySelector("#lista-filmes");
-  listaFilmes.innerHTML = "";
-  console.log(listaFilmes);
-  if(filmes.length > 0) {
-    filmes.forEach(async(filme) => {
-      listaFilmes.appendChild(await filme.getCard());
-    });
-  }
-}
+
