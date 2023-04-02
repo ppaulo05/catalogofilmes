ec1e57421c89e4e637a90ee316ee499c949bb90d
paulo <paulo-engelmann@educar.rs.gov.br>
2023-04-02 14:08:53 -0300

    parte 3.8, falta botao funcionar

--- JS/classes.js
+++ JS/classes.js
@@ -65,15 +65,114 @@ class Filme
 
         return card;
     }
+
+    getBtnDetalhes = () => {
+        return this.btnDetalhes;
+    }
+
     setBtnDetalhes = () => {
         this.btnDetalhes = document.createElement('button');
         this.btnDetalhes.appendChild(document.createTextNode("Detalhes"));
         this.btnDetalhes.setAttribute("id", this.id);
         this.btnDetalhes.setAttribute("class", "btnDetalhesFilme");
       }
-    getBtnDetalhes=()=>{
-    return this.btnDetalhes
-      }
+      getDetalhesFilme =  () => {
 
-    
+        let cardDetalhes = document.createElement('div');
+        cardDetalhes.setAttribute("class", "card-detalhes");
+
+        let imgDetalhes = document.createElement("img");
+        imgDetalhes.setAttribute("class", "img-detalhes");
+        imgDetalhes.setAttribute("src", this.cartaz);
+
+        let cardBodyDetalhes = document.createElement("div");
+        cardBodyDetalhes.setAttribute("class", "body-detalhes");
+
+        let divCabecalho = document.createElement("div");
+        divCabecalho.setAttribute("class", "cabecalho");
+
+        let hTitleDetalhes = document.createElement("h2");
+        hTitleDetalhes.setAttribute("class", "cardTitle");
+        hTitleDetalhes.appendChild(document.createTextNode(this.titulo));
+
+        let salvarFilme = document.createElement('i');
+        salvarFilme.setAttribute("class", "bi bi-star");
+        salvarFilme.setAttribute("id", "salvarDetalhes");
+
+        let fecharFilme = document.createElement('button');
+        fecharFilme.setAttribute("class", "btn-close");
+        fecharFilme.setAttribute("id", this.id);
+        fecharFilme.setAttribute("id", "fecharDetalhes");
+
+        let divDetalhes = document.createElement("div");
+        divDetalhes.setAttribute("class", "div-detalhes");
+
+        let divAno = document.createElement("div");
+        divAno.setAttribute("class", "div-ano");
+        divAno.appendChild(document.createTextNode("• Ano: "));
+
+        let divGenero = document.createElement("div");
+        divGenero.setAttribute("class", "div-genero");
+        divGenero.appendChild(document.createTextNode("• Genêro: "));
+
+        let divDuracao = document.createElement("div");
+        divDuracao.setAttribute("class", "div-duracao");
+        divDuracao.appendChild(document.createTextNode("• Duração: "));
+
+        let divDirecao = document.createElement("div");
+        divDirecao.setAttribute("class", "div-direcao");
+        divDirecao.appendChild(document.createTextNode("• Direção: "));
+
+        let divElenco = document.createElement("div");
+        divElenco.setAttribute("class", "div-elenco");
+        divElenco.appendChild(document.createTextNode("• Elenco: "));
+
+        let divSinopse = document.createElement("div");
+        divSinopse.setAttribute("class", "div-sinopse");
+        divSinopse.appendChild(document.createTextNode("• Sinopse: "));
+
+        let divAvaliacao = document.createElement("div");
+        divAvaliacao.setAttribute("class", "div-avaliacao");
+        divAvaliacao.appendChild(document.createTextNode("• Avaliação: "));
+
+        divAno.appendChild(document.createTextNode(this.ano));
+        divGenero.appendChild(document.createTextNode(this.genero));
+        divDuracao.appendChild(document.createTextNode(this.duracao));
+        divDirecao.appendChild(document.createTextNode(this.direcao));
+        divElenco.appendChild(document.createTextNode(this.elenco));
+        divSinopse.appendChild(document.createTextNode(this.sinopse));
+        divAvaliacao.appendChild(document.createTextNode(this.avaliacao));
+
+        divDetalhes.appendChild(divAno);
+        divDetalhes.appendChild(divGenero);
+        divDetalhes.appendChild(divDuracao);
+        divDetalhes.appendChild(divDirecao);
+        divDetalhes.appendChild(divElenco);
+        divDetalhes.appendChild(divSinopse);
+        divDetalhes.appendChild(divAvaliacao);
+
+        cardDetalhes.appendChild(imgDetalhes);
+        cardDetalhes.appendChild(cardBodyDetalhes);
+
+        divCabecalho.appendChild(hTitleDetalhes);
+        divCabecalho.appendChild(salvarFilme);
+        divCabecalho.appendChild(fecharFilme);
+
+        cardBodyDetalhes.appendChild(divCabecalho);
+        cardBodyDetalhes.appendChild(divDetalhes);
+
+        let btnSalvar = document.createElement('button');
+        btnSalvar.appendChild(document.createTextNode('Favoritar'));
+        btnSalvar.setAttribute("id","btnSalvar");
+        divDetalhes.appendChild(btnSalvar);
+
+        let btnFechar = document.createElement('button');
+        btnFechar.appendChild(document.createTextNode('Fechar'));
+        btnFechar.setAttribute("id","btnFechar");
+        divDetalhes.appendChild(btnFechar);
+
+
+        return cardDetalhes;
+
+    }
 }
--- JS/script.js
+++ JS/script.js
@@ -1,61 +1,4 @@
-//let ator = new Ator(1, "JOHN WAYNE");
-//console.log(ator)
-
-//let diretor = new Diretor(1, "Alfred hitchcock");
-//console.log(diretor);
-
-//let direcao = [
-//    new Diretor(1, "Jim Sheridan"),
-//];
-
-
-//let elenco = [
-//  new Ator(1, " 50 Cent"),
-//new Ator(2, "Adewale Akinnuoye-Agbaje"),
-//new Ator(3, "Joy Bryant"),
-//    new Ator(4, "Terrence Howard"),
-//    new Ator(5, " Ashley Walters"),
-//    new Ator(6, "Omar Benson Miller"),
-//];
-
-
-//let sinopse = "Marcus (50 Cent) é um jovem da periferia que se envolveu em um tiroteio, que por pouco não lhe tirou a vida."
-
-
-//let cartaz = "https://media.fstatic.com/eZPNjnVEoqwltt8b3PJNHHJTuzM=/322x478/smart/filters:format(webp)/media/movies/covers/2017/06/getrich.jpg"
-
-
-//let genero = ["Drama", "Ficção policial"];
-
-
-//let filme = new Filme(
-//   1,
-//    "Fique rico ou morra tentando",
-//    2005,
-//    genero,
-//    135,
-//    sinopse,
-//    cartaz,
-//    direcao,
-//    elenco,
-//    16,
-//    null
-//);
-
-//console.log(filme);
-
-//btnBuscarFilme.onclick = () => {
-//  if (inputBuscarFilme.ariaValueMax.lenght > 0) {
-//      fetch("http://www.omdbapi.com/?apikey=88ffc734&s=" + inputBuscarFilme.value, { mode: "cors" })
-//          .then((resp) => resp.json())
-//          .then((resp) => {
-//              console.log(resp);
-//          });
-//  }
-//  return false;
-//}
-
-let inputBuscarFilme = document.querySelector("#input-buscar-filme");
+/*let inputBuscarFilme = document.querySelector("#input-buscar-filme");
 let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
 
 let listarFilmes = async (filmes) => {
@@ -126,7 +69,7 @@ let detalhesFilme = async (id)=>{
       resp.Awards,
       resp.imdbRating
     )
-    
+
     document.querySelector("#mostrar-filme").appendChild(filme.getDetalhesFilme());
     
     document.querySelector("#btnFechar").onclick = () => {
@@ -141,7 +84,6 @@ let detalhesFilme = async (id)=>{
     document.querySelector("#mostrar-filme").style.display="flex";
   });
 }
-
 
 let filmes = new Array();
 filmesFavoritos.forEach((item) => {
@@ -162,16 +104,113 @@ filmesFavoritos.forEach((item) => {
 filmes.push(filme);
 })
 
-let navFavoritos = document.querySelector("#nav-favoritos");
-navFavoritos.onclick = () => {
-  listarFavoritos();
+//let navFavoritos = document.querySelector("#nav-favoritos");
+//navFavoritos.onclick = () => {
+//  listarFavoritos();
+//}
+
+
+//let filmesString = localStorage.getItem('filmesFavoritos');
+//filmes=JSON.stringify(filmes);
+//localStorage.setItem('filmesFavoritos', filmes);
+
+
+   /* mostrarFilmes.style.display = "flex";
+    mostrarFilmes.appendChild(filme.getDetalhesFilme());
+
+    document.querySelector("#fecharDetalhes").onclick = () => {
+        document.querySelector("#lista-filmes").style.display = "flex";
+        document.querySelector("#mostrar-filmes").innerHTML = "";
+        document.querySelector("#mostrar-filmes").style.display = "none";
+    }
+   */
+
+   
+let inputBuscarFilme = document.querySelector('#input-buscar-filme');
+let btnBuscarFilme = document.querySelector('#btn-buscar-filme');
+let listaFilmes =  document.querySelector("#lista-filmes");
+let mostrarFilmes = document.querySelector('#mostrar-filme');
+let favoritos = document.querySelector('.favoritos');
+
+btnBuscarFilme.onclick = () => {
+    if(inputBuscarFilme.value.length > 0){
+        let filmes = new Array();
+        fetch("http://www.omdbapi.com/?apikey=88ffc734&s="+inputBuscarFilme.value, {mode:"cors"})
+        .then((resp)=> resp.json())
+        .then((resp)=> {
+            resp.Search.forEach((item) => {
+                console.log(item);
+                let filme = new Filme(
+                    item.imdbID,
+                    item.Title,
+                    item.Year,
+                    null,
+                    null,
+                    item.Poster,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                );
+                filmes.push(filme);
+            });
+            listarFilmes(filmes);
+        })
+    }
+    mostrarFilmes.style.display = 'none';
+    return false;
+}
+let listarFilmes = async (filmes) => { 
+    listaFilmes.style.display = 'flex';
+    listaFilmes.innerHTML = "";
+    mostrarFilmes.style.display = 'flex';
+    mostrarFilmes.innerHTML = "";
+    console.log(listaFilmes);
+    if(filmes.length > 0){
+        filmes.forEach(async(filme)=>{
+            console.log(filme);
+            listaFilmes.appendChild(await filme.getCard());
+            filme.getBtnDetalhes().onclick= async()=>{
+                listaFilmes.style.display = 'none';
+                detalhesFilme(filme.id);
+            }
+        })
+    }
 }
 
+let detalhesFilme = async (id) =>{
+    fetch("http://www.omdbapi.com/?apikey=88ffc734&i="+id)
+    .then((resp) => resp.json())
+    .then((resp) => {
+        console.log(resp);
+        let filme = new Filme(
+            resp.imdbID,
+            resp.Title,
+            resp.Year,
+            resp.Genre.split(","),
+            resp.Runtime,
+            resp.Poster,
+            resp.Plot,
+            resp.Director,
+            resp.Actors.split(","),
+            resp.Awards,
+            resp.imdbRating
+        );
 
-let filmesString = localStorage.getItem('filmesFavoritos');
-filmes=JSON.stringify(filmes);
-localStorage.setItem('filmesFavoritos', filmes);
+        mostrarFilmes.style.display = 'flex';
+        mostrarFilmes.appendChild(filme.getCardDetalhes());
 
+        document.querySelector("#btnFechar").onclick = () =>{
+            listaFilmes.style.display="flex";
+            mostrarFilmes.style.display = 'none';
+            mostrarFilmes.innerHTML = "";
+        }
+        document.querySelector("#btnSalvar").onclick = () => {
+            salvarFilmes(filme);
+        }
+    })
+}
 
 
 
