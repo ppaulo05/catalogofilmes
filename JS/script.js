75966c1715ecdb9d01d02c9bcf7d63e717456b75
paulinho car <pauloengelmann158@gmail.com>
2023-04-04 23:47:00 -0300

    parte 4 atualizada

--- JS/script.js
+++ JS/script.js
@@ -10,7 +10,7 @@ let navFavoritos = document.querySelector("#nav-favoritos");
 btnBuscarFilme.onclick = () => {
     if(inputBuscarFilme.value.length > 0){
         let filmes = new Array();
-        fetch("http://www.omdbapi.com/?apikey=88ffc734&s="+inputBuscarFilme.value, {mode:"cors"})
+        fetch("https://www.omdbapi.com/?apikey=88ffc734&s="+inputBuscarFilme.value, {mode:"cors"})
         .then((resp)=> resp.json())
         .then((resp)=> {
             resp.Search.forEach((item) => {
@@ -55,7 +55,7 @@ let listarFilmes = async (filmes) => {
 }
 
 let detalhesFilme = async (id) =>{
-    fetch("http://www.omdbapi.com/?apikey=88ffc734&i="+id)
+    fetch("https://www.omdbapi.com/?apikey=88ffc734&i="+id)
     .then((resp) => resp.json())
     .then((resp) => {
         console.log(resp);
